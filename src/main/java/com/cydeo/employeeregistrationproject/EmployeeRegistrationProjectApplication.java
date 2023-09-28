1198c1483ce0d1e8ec4ad2079c6bf14c6d82bf76
supawadeek <latifetor@gmail.com>
2023-09-28 09:39:01 -0400

    Spring MVC

--- src/main/java/com/cydeo/employeeregistrationproject/EmployeeRegistrationProjectApplication.java
+++ src/main/java/com/cydeo/employeeregistrationproject/EmployeeRegistrationProjectApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeRegistrationProjectApplication {
 
+    // runner_application
+
     public static void main(String[] args) {
         SpringApplication.run(EmployeeRegistrationProjectApplication.class, args);
     }
